e92ea9c007030f4f409f77f7ec24646809cccac8
David Pell <davidcpell@gmail.com>
2016-09-27 22:11:43 -0400

    fix cookstyle offenses

--- resources/rbenv_base.rb
+++ resources/rbenv_base.rb
@@ -3,8 +3,10 @@ resource_name :rbenv_base
 property :prefix, String, name_property: true, default: '/opt'
 property :git_revision, String, default: 'master'
 
-action :install do 
-  git prefix do 
+default_action :install
+
+action :install do
+  git prefix do
     action      :sync
     destination prefix
     repository  'https://github.com/rbenv/rbenv'
