5798ff2b89970bcc551548716149286bea41dbc4
David Pell <davidcpell@gmail.com>
2016-09-28 19:18:27 -0400

    move unit test to into spec/unit/recipes

--- spec/unit/recipes/rbenv_base_spec.rb
+++ spec/unit/recipes/rbenv_base_spec.rb
@@ -6,18 +6,18 @@ describe 'resource_fixtures::rbenv_base' do
   end
 
   context 'when a prefix is specified but everything else is default' do
-    let(:prefix) { '/test_prefix' }
+    let(:target) { '/opt/.rbenv' }
     let(:basic_git_opts) { { action: [:sync], revision: 'master' } }
 
     it 'clones the rbenv repo' do
-      rbenv_opts = basic_git_opts.merge(destination: prefix, repository: 'https://github.com/rbenv/rbenv')
+      rbenv_opts = basic_git_opts.merge(destination: target, repository: 'https://github.com/rbenv/rbenv')
 
-      expect(chef_run).to sync_git('/test_prefix').with(rbenv_opts)
+      expect(chef_run).to sync_git('/opt/.rbenv').with(rbenv_opts)
     end
 
     it 'installs ruby-build' do
-      expect(chef_run).to create_directory('/test_prefix/.rbenv/plugins')
-      expect(chef_run).to sync_git('/test_prefix/.rbenv/plugins/ruby-build').with(basic_git_opts)
+      expect(chef_run).to create_directory('/opt/.rbenv/plugins')
+      expect(chef_run).to sync_git('/opt/.rbenv/plugins/ruby-build').with(basic_git_opts)
     end
   end
 end
