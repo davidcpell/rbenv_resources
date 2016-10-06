e84efc92b3788f64bbd36290c59020a96e2d8490
David Pell <davidcpell@gmail.com>
2016-10-06 19:45:18 -0400

    change how commands are tested

--- test/recipes/global_ruby_spec.rb
+++ test/recipes/global_ruby_spec.rb
@@ -4,3 +4,7 @@ describe directory('/opt/rbenv/version') do
   it { should be_file }
   its('content') { should eq "2.2.2\n" }
 end
+
+describe command('bash -l -c "rbenv global"') do 
+  its('stdout') { should eq "2.2.2\n" }
+end
--- test/recipes/ruby_version_spec.rb
+++ test/recipes/ruby_version_spec.rb
@@ -4,6 +4,6 @@ describe directory('/opt/rbenv/versions/2.2.2') do
   it { should be_directory }
 end
 
-describe command('/opt/rbenv/bin/rbenv versions') do 
+describe command('bash -l -c "rbenv versions"') do 
   its('stdout') { should include '2.2.2' }
 end
