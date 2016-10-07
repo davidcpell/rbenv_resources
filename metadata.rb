0aa59b5c8db85b6d83fbb1d123216ed2da5f77de
David Pell <davidcpell@gmail.com>
2016-10-07 12:27:12 -0400

    add source_url to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ maintainer_email 'davidcpell@gmail.com'
 license 'all_rights'
 description 'Provides resources for installing/configuring rbenv'
 long_description 'Provides resources for installing/configuring rbenv'
-version '0.6.4'
+version '0.6.5'
 
 supports 'ubuntu', '>= 14.04'
 supports 'centos', '= 7.2'
+
+source_url 'https://github.com/davidcpell/rbenv_resources'
