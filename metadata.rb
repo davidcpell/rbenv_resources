8f3e5e9ab7825e1757218b319eec660ab9b20fe6
David Pell <davidcpell@gmail.com>
2016-10-07 11:57:20 -0400

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'davidcpell@gmail.com'
 license 'all_rights'
 description 'Provides resources for installing/configuring rbenv'
 long_description 'Provides resources for installing/configuring rbenv'
-version '0.6.1'
+version '0.6.2'
 
 supports 'ubuntu', '>= 14.04'
 supports 'centos', '= 7.2'
